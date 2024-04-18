f456746bf7233d230357f8a74eea10a2de057c66
JaviRute <jrute@live.com>
2024-04-18 20:24:55 +0100

    correct tutorial

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -15,8 +15,8 @@ export default function Tutorial(props) {
         <h4>Teacher Mode</h4>
         <p>You can toggle between Student and Teacher Mode with the top right button.</p>
         <p>Select the desired tenses and persons and set the timer (20 seconds by default).</p>
-        <p>When you click 'Play', a random infinitive, tense and person are selected, and the cheatsheet will appear automatically.</p>
-        <p>Ask your students and click on the 'Show Answer' button to see whether they deserve an AP.</p>
+        <p>When you click on 'Play' (or press '/'), a random infinitive, tense and person are selected, and the cheatsheet will appear automatically.</p>
+        <p>Ask your students and click on the 'Show Answer' button (or click '.') to see whether they deserve an AP.</p>
       </div>
     </div>
   )
