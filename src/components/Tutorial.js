b06b9ceb8ed165325ae4051d2962409b59c08a1a
JaviRute <jrute@live.com>
2024-04-19 20:12:17 +0100

    add .eslintrc.json

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -7,7 +7,7 @@ export default function Tutorial(props) {
       <div className='modal'>
         <h3>Welcome to the Spanish Verbs Trainer!</h3>
         <h4>Student Mode</h4>
-        <p>Click on the 'Play' button (or press '/' on your Keyboard) and you will be given a random infinitive, verb tense and person (I form, you form etc).</p>
+        <p>Click on the 'Play' button (or press '/' on your Keyboard) and you will be given a random infinitive, verb tense and person (I form, you form, etc).</p>
         <p>Remember you can always click on the 'Help' button to see a cheatsheet of the current tense.</p>
         <p>Once you have the solution you can click on the 'Check' button (or press Enter on your Keyboard).</p>
         <p>You have 3 tries each time.</p>
