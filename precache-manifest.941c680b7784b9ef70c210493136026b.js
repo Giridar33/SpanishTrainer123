77acd6195489bbef70fac243f4f72dc746296c61
Giridar33 <giridar.3@gmail.com>
2025-04-13 15:23:01 +0530

    Updates

---
+++ precache-manifest.941c680b7784b9ef70c210493136026b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99123b17208b7f880aae0adabeaedb74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "095fbaebeedb12430c58",
+    "url": "/static/css/main.fba3aec2.chunk.css"
+  },
+  {
+    "revision": "da6f46d19e84df4c1f67",
+    "url": "/static/js/2.21b7a924.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "095fbaebeedb12430c58",
+    "url": "/static/js/main.4565b2da.chunk.js"
+  },
+  {
+    "revision": "8a5626378c6138c21833",
+    "url": "/static/js/runtime~main.cef9ab07.js"
+  }
+]);
